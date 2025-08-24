453dcd92154306d39049ddfe8e97650c4658d166
aqibullah <aqibullah3312@gmail.com>
2025-08-24 14:57:27 +0500

    removed unused code

--- resources/js/sections/BookingOptionsSection.vue
+++ resources/js/sections/BookingOptionsSection.vue
@@ -5,7 +5,8 @@ import cricketImage from '@images/categories/cricket.jpg';
 import gymImage from '@images/categories/gym.jpg';
 import swimmingImage from '@images/categories/swimming.jpg';
 import kidsImage from '@images/categories/kids-2.jpg'; // Assume you have an image for Kids Area
-import { Card,CardContent } from '@/components/ui/card'; // Assume Kids Zone image exists
+import { Card,CardContent } from '@/components/ui/card';
+import { Link } from '@inertiajs/vue3'; // Assume Kids Zone image exists
 
 const facilities = [
     {
