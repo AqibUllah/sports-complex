ebc3875c84dfd681f1ff98cf5139d52e39fb2589
Aqib Ullah <aqibullah3312@gmail.com>
2025-08-26 12:42:48 +0500

    removed unused code + design modified

--- resources/js/sections/VenuesSection.vue
+++ resources/js/sections/VenuesSection.vue
@@ -6,7 +6,8 @@ import footballImage from '@images/categories/football.jpg';
 import cricketImage from '@images/categories/cricket.jpg';
 import gymImage from '@images/categories/gym.jpg';
 import swimmingImage from '@images/categories/swimming.jpg';
-import { Card,CardContent,CardFooter,CardAction } from '@/components/ui/card'; // Assume Kids Zone image exists
+import { Card,CardContent } from '@/components/ui/card'; // Assume Kids Zone image exists
+import { Icon } from "@iconify/vue";
 
 // Reactive state for selected sport tab
 const selectedSport = ref('Football');
@@ -158,54 +159,47 @@ const selectSport = (sport: string) => {
                 <Card
                     v-for="venue in venues[selectedSport]"
                     :key="venue.title"
-                    class="bg-gray-100 backdrop-blur-lg shadow-md rounded-xl overflow-hidden hover:scale-105 transition-transform duration-300"
+                    class="bg-gray-100 backdrop-blur-lg shadow-md rounded-xl overflow-hidden transition-transform duration-300 card-hover flex flex-col h-full"
                     >
                     <CardContent>
-                        <div class="flex h-55">
-                            <img
-                                :src="venue.image"
-                                :alt="venue.title"
-                                class="w-full h-66 object-cover"
-                            />
-                            <div class="w-full">
-                                <div class="p-2 flex-1">
-                                    <h3 class="text-md sm:text-lg font-semibold mb-2 text-gray-800">{{ venue.title }}</h3>
-                                    <div class="flex items-center justify-between mb-2  border border-primary-dark px-2 rounded-md max-w-[60px]">
-                                        <svg
-                                            class="w-5 h-5 text-yellow-400 mr-1"
-                                            fill="currentColor"
-                                            viewBox="0 0 20 20"
-                                        >
-                                            <path
-                                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                                            />
-                                        </svg>
-                                        <span class="text-sm sm:text-base text-gray-500">{{ venue.rating }}</span>
+                        <div class="flex-1">
+                            <div class="flex">
+                                <img
+                                    :src="venue.image"
+                                    :alt="venue.title"
+                                    class="w-1/2 h-46 object-cover"
+                                />
+                                <div class="w-full">
+                                    <div class="p-2 flex-1">
+                                        <h3 class="text-md sm:text-lg font-semibold mb-2 text-gray-800">{{ venue.title }}</h3>
+                                        <div class="flex items-center justify-between mb-2  border border-primary-dark px-2 rounded-md max-w-[60px]">
+                                            <svg
+                                                class="w-5 h-5 text-yellow-400 mr-1"
+                                                fill="currentColor"
+                                                viewBox="0 0 20 20"
+                                            >
+                                                <path
+                                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                                                />
+                                            </svg>
+                                            <span class="text-sm sm:text-base text-gray-500">{{ venue.rating }}</span>
+                                        </div>
+                                        <p class="text-xs sm:text-sm text-gray-600 mb-4 line-clamp-2">
+                                            {{ venue.description }}
+                                        </p>
+                                        <span class="md:text-4xl text-gray-600 sm:text-base mb-4">{{ venue.price }}<span class="text-lg">/hr</span></span>
                                     </div>
-                                    <p class="text-xs sm:text-sm text-gray-600 mb-4 line-clamp-2">
-                                        {{ venue.description }}
-                                    </p>
-                                    <span class="md:text-4xl text-gray-600 sm:text-base mb-4">{{ venue.price }}<span class="text-lg">/hr</span></span>
                                 </div>
-                                <Link
-                                    :href="'#'"
-                                    class="bg-primary-dark w-full text-white px-3 py-1.5 sm:px-4 sm:py-2 rounded-br-md hover:bg-primary-light text-sm sm:text-base inline-block"
-                                >
-                                    Book Now
-                                </Link>
                             </div>
+                            <Link
+                                :href="'#'"
+                                class="mt-auto bg-primary-dark hover:bg-primary-light transition-all duration-300 text-gray-100 px-4 py-3 flex items-center justify-between"
+                                >
+                                <span>Book Now</span>
+                                <Icon icon="line-md:chevron-small-double-right" class="text-gray-100" width="25" height="25" />
+                            </Link>
                         </div>
                     </CardContent>
-<!--                    <CardFooter class="mb-4">-->
-<!--                        <CardAction>-->
-<!--                            <Link-->
-<!--                                :href="'#'"-->
-<!--                                class="bg-primary-dark text-white px-3 py-1.5 sm:px-4 sm:py-2 rounded-md hover:bg-primary-light text-sm sm:text-base inline-block"-->
-<!--                            >-->
-<!--                                Book Now-->
-<!--                            </Link>-->
-<!--                        </CardAction>-->
-<!--                    </CardFooter>-->
                 </Card>
             </div>
         </div>
