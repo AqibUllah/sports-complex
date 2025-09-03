531a100a8fb26424f60f5d66bd3032467c85cc1d
Aqib Ullah <aqibullah3312@gmail.com>
2025-09-03 14:42:00 +0500

    completed

--- app/Enums/DurationType.php
+++ app/Enums/DurationType.php
@@ -2,16 +2,19 @@
 
 namespace App\Enums;
 
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasDescription;
+use Filament\Support\Contracts\HasIcon;
 use Filament\Support\Contracts\HasLabel;
 use Illuminate\Contracts\Support\Htmlable;
 
-enum DurationType implements  HasLabel
+enum DurationType: string implements  HasLabel, HasColor, HasIcon, HasDescription
 {
 
-    const string PerVisit = 'per_visit';
-    const string Monthly = 'monthly';
-    const string Yearly = 'yearly';
-    const string Custom = 'custom';
+    case PerVisit = 'per_visit';
+    case Monthly = 'monthly';
+    case Yearly = 'yearly';
+    case Custom = 'custom';
 
     #[\Override] public function getLabel(): string|Htmlable|null
     {
@@ -23,6 +26,36 @@ enum DurationType implements  HasLabel
             self::Yearly => 'Yearly',
             self::Custom => 'Custom',
             default => $this->name, // Fallback to the enum case name
+        };
+    }
+
+    public function getColor(): string | array | null
+    {
+        return match ($this) {
+            self::PerVisit => 'gray',
+            self::Monthly => 'warning',
+            self::Yearly => 'success',
+            self::Custom => 'danger',
+        };
+    }
+
+    public function getIcon(): ?string
+    {
+        return match ($this) {
+            self::PerVisit => 'heroicon-m-pencil',
+            self::Monthly => 'heroicon-m-eye',
+            self::Yearly => 'heroicon-m-check',
+            self::Custom => 'heroicon-m-x-mark',
+        };
+    }
+
+    public function getDescription(): ?string
+    {
+        return match ($this) {
+            self::PerVisit => 'daily membership on per visit',
+            self::Monthly => 'Monthly membership',
+            self::Yearly => 'Yearly membership',
+            self::Custom => 'user can buy membership with custom reuirements.',
         };
     }
 }
