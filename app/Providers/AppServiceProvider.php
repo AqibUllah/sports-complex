9b9101482a1975bb2039c67536501e1fd3555e50
aqibullah <aqibullah3312@gmail.com>
2025-09-07 13:46:56 +0500

    modified

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Dedoc\Scramble\Scramble;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\Route;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -32,5 +33,7 @@ class AppServiceProvider extends ServiceProvider
                 ui: '/docs/v1/api',
                 document: '/docs/v1/openapi.json',
             );
+
+        Schema::defaultStringLength(191);
     }
 }
