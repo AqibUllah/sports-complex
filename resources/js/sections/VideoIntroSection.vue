1de0312bf7e212931d420940d8f01d7091462eda
aqibullah <aqibullah3312@gmail.com>
2025-09-08 08:58:49 +0500

    design modified

--- resources/js/sections/VideoIntroSection.vue
+++ resources/js/sections/VideoIntroSection.vue
@@ -15,7 +15,7 @@ import { Link } from '@inertiajs/vue3';
                 preload="none"
                 class="w-[2000px] h-auto rounded-md"
             >
-                <source src="@videos/intro-2.mp4" type="video/mp4" />
+                <source src="@videos/intro-3.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <!-- Intro Text Overlay -->
@@ -23,18 +23,18 @@ import { Link } from '@inertiajs/vue3';
                     class="animate__animated animate__fadeInUp hidden lg:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white/10 backdrop-blur-lg border border-white/10 shadow-md rounded-xl my-8 p-4 sm:p-6 md:p-8 w-full max-w-sm sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-2xl mx-auto"
                 >
                     <!-- Heading -->
-                    <h2
-                        class="text-xs sm:text-sm md:text-lg lg:text-xl font-bold text-gray-800 uppercase tracking-wide"
+                    <h1
+                        class="text-xs sm:text-sm md:text-lg dark:text-white lg:text-2xl font-bold text-gray-800 uppercase tracking-wide"
                     >
                         Welcome to
-                    </h2>
+                    </h1>
                     <h1
                         class="text-lg sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-primary-dark mt-1 sm:mt-2"
                     >
                         Uptown Arena Sports Complex
                     </h1>
                     <p
-                        class="mt-2 mb-5 sm:mt-3 text-xs sm:text-sm md:text-base lg:text-lg text-gray-900 max-w-full sm:max-w-md md:max-w-lg"
+                        class="mt-2 mb-5 sm:mt-3 text-xs sm:text-sm md:text-base dark:text-white lg:text-lg text-gray-900 max-w-full sm:max-w-md md:max-w-lg"
                     >
                         Your ultimate destination for sports and fitness. Book facilities, join memberships, and explore our categories.
                     </p>
